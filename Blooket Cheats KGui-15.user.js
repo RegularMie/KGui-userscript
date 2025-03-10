678f82693bf8e83a857fb1b82892c3f7cc7f045c
RegularMie <lilbaconlolz@gmail.com>
2025-03-10 23:04:22 +0700

    Update Blooket Cheats KGui-15.user.js

--- Blooket Cheats KGui-15.user.js
+++ Blooket Cheats KGui-15.user.js
@@ -5,7 +5,7 @@
 // @description  Blooket Cheats
 // @author       DannyDan0167 and Cool Duck
 // @match        *://*.blooket.com/*
-// @icon         https://i.ibb.co/sWqBm0K/1024.png
+// @icon         https://github.com/RegularMie/KGui-userscript/blob/main/image%20KGui.png
 // @grant        none
 // @require     https://raw.githubusercontent.com/Blooket-Council/Blooket-Cheats/refs/heads/main/cheats/KGui.js
 // ==/UserScript==
