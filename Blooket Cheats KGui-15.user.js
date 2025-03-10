6b8bff7a4390a027348d08420522a702d8de7b4f
RegularMie <lilbaconlolz@gmail.com>
2025-03-10 18:56:42 +0700

    Add files via upload

---
+++ Blooket Cheats KGui-15.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Blooket Cheats KGui
+// @namespace    https://github.com/randomstuff69/blooketcheatsplus
+// @version      15
+// @description  Blooket Cheats
+// @author       DannyDan0167 and Cool Duck
+// @match        *://*.blooket.com/*
+// @icon         https://i.ibb.co/sWqBm0K/1024.png
+// @grant        none
+// @require     https://raw.githubusercontent.com/Blooket-Council/Blooket-Cheats/refs/heads/main/cheats/KGui.js
+// ==/UserScript==
